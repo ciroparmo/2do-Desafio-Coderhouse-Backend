b4632a40024ad93d77ef23d6cbc8994749ea4212
Ciro Parmo <136656585+ciroparmo@users.noreply.github.com>
2024-02-18 16:46:53 -0300

    0.0.0

--- app.js
+++ app.js
@@ -1,38 +1,19 @@
-class ProductManager {
-    products = []
-
-    addProduct = (title, description, price, code, stock) => {
-        const product = {
-            title: title,
-            description: description,
-            price: price,
-            code: code,
-            stock: stock
-        }
-
-        const repeatedProduct = this.products.find(prod => prod.code === product.code)
-
-        if (!repeatedProduct) {
-            this.products.push(product)
-        } else {
-            console.log(`Error: El código ${product.code} ya existe. No se puede agregar el producto.`);
-        }
-
-    }
-
-    getProducts() {
-        console.log(this.products)
-    }
-}
+const ProductManager = require('./ProductManager.js')
 
 const manager = new ProductManager()
 
-function getRandomNumber() {
-    let randomNumber = Math.floor(100 + Math.random() * 900)
+function randomNumber() {
+    const random = Math.floor(Math.random() * (999 - 100 + 1)) + 100;
 
-    return randomNumber
+    return random
 }
 
-manager.addProduct('BMW', 'Auto Rapido', 5000, getRandomNumber(), 5)
-manager.addProduct('Audi', 'Auto Superrapido', 12500, getRandomNumber(), 3)
-manager.addProduct('Volkswagen', 'Auto Comodo', 1000, getRandomNumber(), 25)
+let persistirProducto = async () => {
+    await manager.addProduct(randomNumber(), 'Audi', 'Auto Rapido', 12000, 5)
+    await manager.addProduct(randomNumber(), 'Ferrari', 'Auto Deportivo', 80000, 1)
+    await manager.addProduct(randomNumber(), 'Porsche', 'Auto Lujoso', 57000, 10)
+    let productos = await manager.getProducts()
+    console.log(productos)
+}
+
+persistirProducto()
